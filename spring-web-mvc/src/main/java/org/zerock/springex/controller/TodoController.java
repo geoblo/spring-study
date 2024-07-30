d5dcc830f663ee01117d5112fd81defbea128067
geoblo <geoblo@naver.com>
2024-07-30 17:08:28 +0900

    7/30 1st

--- spring-web-mvc/src/main/java/org/zerock/springex/controller/TodoController.java
+++ spring-web-mvc/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -1,17 +1,25 @@
 package org.zerock.springex.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.springex.dto.TodoDTO;
+import org.zerock.springex.service.TodoService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/todo") // 특정한 경로의 요청(Request)을 지정, 클래스의 선언부와 메소드에 사용 가능
 @Log4j2
+@RequiredArgsConstructor
 public class TodoController {
+    private final TodoService todoService;
 
     @RequestMapping("/list") // 최종 경로는 '/todo/list'
     public void list() {
@@ -29,9 +37,20 @@ public class TodoController {
     }
 
     @PostMapping("/register")
-    public String registerPost(TodoDTO todoDTO) { // 객체 자료형의 파라미터 수집
+    public String registerPost(@Valid TodoDTO todoDTO, // 객체 자료형의 파라미터 수집
+                               BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes) {
         log.info("POST todo register....");
+
+        if (bindingResult.hasErrors()) {
+            log.info("has errors......");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/todo/register";
+        }
+
         log.info(todoDTO);
+
+        todoService.register(todoDTO);
 
         return "redirect:/todo/list";
     }
--- spring-web-mvc/src/main/webapp/WEB-INF/views/todo/register.jsp
+++ spring-web-mvc/src/main/webapp/WEB-INF/views/todo/register.jsp
@@ -6,6 +6,7 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Title</title>
@@ -87,5 +88,16 @@
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <script>
+        // 자바스크립트를 이용해서 오류 객체를 생성하면 나중에 화면에서 자유롭게 처리가 가능한 장점이 있음
+        const serverValidResult = {};
+
+        <c:forEach items="${errors}" var="error">
+            serverValidResult['${error.getField()}'] = '${error.defaultMessage}';
+        </c:forEach>
+
+        console.log(serverValidResult);
+    </script>
 </body>
 </html>
